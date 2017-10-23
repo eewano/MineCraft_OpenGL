cad3dea6bef449a06bb38c801587881560dded74
eewano <awanoyasutaka@AwanonoMacBook-puro.local>
2017-10-23 11:38:34 +0900

    コードの修正。 Playerフォルダ関連のコードが完了。

--- MineCraft/Source/Player/Player.cpp
+++ MineCraft/Source/Player/Player.cpp
@@ -1,53 +1,203 @@
 #include "Player.hpp"
 
-Player::Player() {
-    position = {0, 0, 5};
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include "../world/World.hpp"
+
+#include "../Renderer/RenderMaster.hpp"
+
+sf::Font f;
+
+Player::Player()
+        : Entity({2500, 125, 2500}, {0, 0, 0}, {0.3, 1.0, 0.3}),
+          m_itemDown(sf::Keyboard::Down),
+          m_itemUp(sf::Keyboard::Up),
+          m_flyKey(sf::Keyboard::F) {
+    for (int i =; i < 5; i++) {
+        m_inventoryJumps.emplace_back((sf::Keyboard::Key(sf::Keyboard::Num1 + i)));
+    }
+
+    f.loadFromFile("Res/Fonts/rs.ttf");
+
+    for (int i = 0; i < 5; i++) {
+        m_items.emplace_back(Material::NOTHING, 0);
+    }
+
+    for (float i = 0; i < 5; i++) {
+        sf::Text t;
+        t.setFont(f);
+        t.setOutlineColor(sf::Color::Black);
+        t.setCharacterSize(25);
+        t.setPosition({20.0f, 20.0f * i + 100.0f});
+        m_itemText.push_back(t);
+    }
+    m_posPrint.setFont(f);
+    m_posPrint.setOutlineColor(sf::Color::Black);
+    m_posPrint.setCharacterSize(25);
+    m_posPrint.setPosition(20.0f, 20.0f * 6.0f + 100.0f);
+}
+
+void Player::addItem(const Material &material) {
+    Material::ID id = material.id;
+
+    for (unsigned i = 0; i < m_items.size(); i++) {
+        if (m_items[i].getMaterial().id == id) {
+            /*int leftOver =*/ m_items[i].add(1);
+
+            return;
+        } else if (m_items[i].getMaterial().id == Material::ID::Nothing) {
+            m_items[i] = {material, 1};
+            return;
+        }
+    }
+}
+
+ItemStack &Player::getHeldItems() {
+    return m_items[m_heldItem];
 }
 
 void Player::handleInput(const sf::RenderWindow &window) {
     keyboardInput();
     mouseInput(window);
+
+    if (m_itemDown.isKeyPressed()) {
+        m_heldItem++;
+        if (m_heldItem == (int) m_items.size()) {
+            m_heldItem = 0;
+        }
+    } else if (m_itemUp.isKeyPressed()) {
+        m_heldItem--;
+        if (m_heldItem == -1) {
+            m_heldItem = m_items.size() - 1;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (m_inventoryJumps[i].isKeyPressed()) {
+            m_heldItem = i;
+        }
+    }
+
+    if (m_flyKey.isKeyPressed()) {
+        m_isFlying = !m_isFlying;
+    }
 }
 
-void Player::update(float dt) {
-    position += m_velocity * dt;
-    m_velocity *= 0.95;
+void Player::update(float dt, World &world) {
+    velocity += m_acceleration;
+    m_acceleration = {0, 0, 0};
+
+    if (!m_isFlying) {
+        if (!m_isOnGround) {
+            velocity.y -= 40 * dt;
+        }
+        m_isOnGround = false;
+    }
+
+    position.x += velocity.x * dt;
+    collide(world, {velocity.x, 0, 0}, dt);
+
+    position.y += velocity.y * dt;
+    collide(world, {0, velocity.y, 0}, dt);
+
+    position.z += velocity.z * dt;
+    collide(world, {0, 0, velocity.z}, dt);
+
+    box.update(position);
+    velocity.x *= 0.95;
+    velocity.z *= 0.95;
+    if (m_isFlying) {
+        velocity.y *= 0.95;
+    }
 }
+
+void Player::collide(World &world, const glm::vec3 &vel, float dt) {
+    for (int x = position.x - box.dimensions.x; x < position.x + box.dimensions.x; x++)
+        for (int y = position.y - box.dimensions.y; y < position.y + 0.7; y++)
+            for (int z = position.z - box.dimensions.z; z < position.z + box.dimensions.z; z++) {
+                auto block = world.getBlock(x, y, z);
+
+                if (block != 0 && block.getData().isCollidable) {
+                    if (vel.y > 0) {
+                        position.y = y - box.dimensions.y;
+                        velocity.y = 0;
+                    } else if (vel.y < 0) {
+                        m_isOnGround = true;
+                        position.y = y + box.dimensions.y + 1;
+                        velocity.y = 0;
+                    }
+
+                    if (vel.x > 0) {
+                        position.x = x - box.dimensions.x;
+                    } else if (vel.x < 0) {
+                        position.x = x + box.dimensions.x + 1;
+                    }
+
+                    if (vel.z > 0) {
+                        position.z = z - box.dimensions.z;
+                    } else if (vel.z < 0) {
+                        position.z = z + box.dimensions.z + 1;
+                    }
+                }
+            }
+}
+
+///@TODO Move this
+float speed = 0.2f;
 
 void Player::keyboardInput() {
-    glm::vec3 change;
-    float speed = 0.5;
-
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        change.x += -glm::cos(glm::radians(rotation.y + 90)) * speed;
-        change.z += -glm::sin(glm::radians(rotation.y + 90)) * speed;
+        float s = speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) {
+            s *= 5;
+        }
+        m_acceleration.x += -glm::cos(glm::radians(rotation.y + 90)) * s;
+        m_acceleration.z += -glm::sin(glm::radians(rotation.y + 90)) * s;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        change.x += glm::cos(glm::radians(rotation.y + 90)) * speed;
-        change.z += glm::sin(glm::radians(rotation.y + 90)) * speed;
+        m_acceleration.x += glm::cos(glm::radians(rotation.y + 90)) * speed;
+        m_acceleration.z += glm::sin(glm::radians(rotation.y + 90)) * speed;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        change.x += -glm::cos(glm::radians(rotation.y)) * speed;
-        change.z += -glm::sin(glm::radians(rotation.y)) * speed;
+        m_acceleration.x += -glm::cos(glm::radians(rotation.y)) * speed;
+        m_acceleration.z += -glm::sin(glm::radians(rotation.y)) * speed;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        change.x += glm::cos(glm::radians(rotation.y)) * speed;
-        change.z += glm::sin(glm::radians(rotation.y)) * speed;
+        m_acceleration.x += glm::cos(glm::radians(rotation.y)) * speed;
+        m_acceleration.z += glm::sin(glm::radians(rotation.y)) * speed;
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-        change.y += speed;
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-        change.y -= speed;
+        jump();
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) && m_isFlying) {
+        m_acceleration.y -= speed * 3;
     }
-
-    m_velocity += change;
 }
 
 void Player::mouseInput(const sf::RenderWindow &window) {
-    static auto const BOUND = 80;
+    static bool useMouse = true;
+    static ToggleKey useMouseKey(sf::Keyboard::L);
+
+    if (useMouseKey.isKeyPressed()) {
+        useMouse = !useMouse;
+    }
+
+    if (!useMouse) {
+        return;
+    }
+
+    static sf::Vector2i center = {
+            static_cast<int>(window.getSize().x / 2),
+            static_cast<int>(window.getSize().y / 2)
+    };
+
+    static auto const BOUND = 89.9999;
     static auto lastMousePosition = sf::Mouse::getPosition(window);
-    auto change = sf::Mouse::getPosition() - lastMousePosition;
+    auto change = sf::Mouse::getPosition(window) - lastMousePosition;
 
     rotation.y += change.x * 0.05;
     rotation.x += change.y * 0.05;
@@ -64,10 +214,43 @@ void Player::mouseInput(const sf::RenderWindow &window) {
         rotation.y = 360;
     }
 
-    auto cx = static_cast<int>(window.getSize().x / 4);
-    auto cy = static_cast<int>(window.getSize().y / 4);
+    lastMousePosition = sf::Mouse::getPosition(window);
 
-    sf::Mouse::setPosition({cx, cy}, window);
+    if (lastMousePosition.x < 10 || lastMousePosition.x > window.getSize().x - 10 || lastMousePosition.y < 10 || lastMousePosition.y > window.getSize().y - 10) {
+        sf::Mouse::setPosition(center);
+        lastMousePosition = center;
+    }
+}
 
-    lastMousePosition = sf::Mouse::getPosition();
+void Player::draw(RenderMaster &master) {
+    for (unsigned i = 0; i < m_items.size(); i++) {
+        sf::Text &t = m_itemText[i];
+        if (i == (unsigned) m_heldItem) {
+            t.setFillColor(sf::Color::Red);
+        } else {
+            t.setFillColor(sf::Color::White);
+        }
+        t.setString((m_items[i].getMaterial().name) + " " + std::to_string(m_items[i].getNumInStack()) + " ");
+        master.drawSFML(t);
+    }
+    std::ostringstream stream;
+    stream << " X: " << position.x
+           << " Y: " << position.y
+           << " Z: " << position.z
+           << " Grounded " << std::boolalpha << m_isOnGround;
+
+    m_posPrint.setString(stream.str());
+
+    master.drawSFML(m_posPrint);
+}
+
+void Player::jump() {
+    if (!m_isFlying) {
+        if (m_isOnGround) {
+            m_isOnGround = false;
+            m_acceleration.y += speed * 50;
+        }
+    } else {
+        m_acceleration.y += speed * 3;
+    }
 }
--- MineCraft/Source/Player/Player.hpp
+++ MineCraft/Source/Player/Player.hpp
@@ -2,8 +2,15 @@
 #define Player_hpp
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 #include "../Entity.h"
+#include "../Item/ItemStack.hpp"
+#include "../ToggleKey.hpp"
+
+class World;
+
+class RenderMaster;
 
 class Player : public Entity {
 public:
@@ -11,14 +18,42 @@ public:
 
     void handleInput(const sf::RenderWindow &window);
 
-    void update(float dt);
+    void update(float dt, World &world);
+
+    void collide(World &world, const glm::vec3 &vel, float dt);
+
+    void addItem(const Material &material);
+
+    void draw(RenderMaster &master);
+
+    ItemStack &getHeldItems();
 
 private:
+    void jump();
+
     void keyboardInput();
 
     void mouseInput(const sf::RenderWindow &window);
 
-    glm::vec3 m_velocity;
+    bool m_isOnGround = false;
+
+    bool m_isFlying = false;
+
+    std::vector<ItemStack> m_items;
+
+    std::vector<sf::Text> m_itemText;
+
+    sf::Text m_posPrint;
+
+    int m_heldItem = 0;
+
+    ToggleKey m_itemDown;
+    ToggleKey m_itemUp;
+    ToggleKey m_flyKey;
+
+    std::vector<ToggleKey> m_inventoryJumps;
+
+    glm::vec3 m_acceleration;
 };
 
 #endif /* Player_hpp */
