2a17a60132a55a0c7de00183586a6c2ad50cd746
eewano <awanoyasutaka@AwanonoMacBook-puro.local>
2017-10-24 14:00:50 +0900

    コードを修正。 World/Generation/Terrainフォルダ関連のコードが完了。

--- MineCraft/Source/World/Generation/Terrain/ClassicOverWorldGenerator.hpp
+++ MineCraft/Source/World/Generation/Terrain/ClassicOverWorldGenerator.hpp
@@ -11,5 +11,47 @@
 
 #include "../Biome/GrasslandBiome.hpp"
 #include "../Biome/TemperateForestBiome.hpp"
+#include "../Biome/DesertBiome.hpp"
+#include "../Biome/OceanBiome.hpp"
+#include "../Biome/LightForest.hpp"
+
+class Chunk;
+
+class ClassicOverWorldGenerator : public TerrainGenerator {
+public:
+    ClassicOverWorldGenerator();
+
+    void generateTerrainFor(Chunk &chunk) override;
+
+    int getMinimumSpawnHeight() const noexcept override;
+
+private:
+    static void setUpNoise();
+
+    void setBlocks(int maxHeight);
+
+    void getHeightIn(int xMin, int zMin, int xMax, int zMax);
+
+    void getHeightMap();
+
+    void getBiomeMap();
+
+    const Biome &getBiome(int x, int z) const;
+
+    Array2D<int, CHUNK_SIZE> m_heightMap;
+    Array2D<int, CHUNK_SIZE + 1> m_biomeMap;
+
+    Random<std::minstd_rand> m_random;
+
+    static NoiseGenerator m_biomeNoiseGen;
+
+    GrasslandBiome m_grassBiome;
+    TemperateForestBiome m_temperateForest;
+    DesertBiome m_desertBiome;
+    OceanBiome m_oceanBiome;
+    LightForest m_lightForest;
+
+    Chunk *m_pChunk = nullptr;
+};
 
 #endif /* ClassicOverWorldGenerator_hpp */
